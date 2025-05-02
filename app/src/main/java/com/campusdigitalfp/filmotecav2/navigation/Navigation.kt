faffe4567f0577d51485b5816cf83bbf08629b5a
Carlos Garcés Bordegé <CGarces@users.noreply.github.com>
2025-05-02 23:21:56 +0200

    Inicio de Sesión con Firebase, Authentication

--- app/src/main/java/com/campusdigitalfp/filmotecav2/navigation/Navigation.kt
+++ app/src/main/java/com/campusdigitalfp/filmotecav2/navigation/Navigation.kt
@@ -9,24 +9,56 @@ import com.campusdigitalfp.filmotecav2.screens.AboutScreen
 import com.campusdigitalfp.filmotecav2.screens.FilmDataScreen
 import com.campusdigitalfp.filmotecav2.screens.FilmEditScreen
 import com.campusdigitalfp.filmotecav2.screens.FilmListScreen
+import com.campusdigitalfp.filmotecav2.screens.LoginScreen
+import com.campusdigitalfp.filmotecav2.viewmodel.AuthViewModel
 import com.campusdigitalfp.filmotecav2.viewmodel.FilmViewModel
+import com.google.firebase.auth.FirebaseAuth
+
+fun comprobarUsuario(): Boolean {
+    val auth = FirebaseAuth.getInstance()
+    val currentUser = auth.currentUser
+    return currentUser != null
+}
 
 @Composable
-fun Navigation(viewModel: FilmViewModel) {
+fun Navigation(viewModel: FilmViewModel, authViewModel: AuthViewModel) {
     val navController = rememberNavController()
+    // Define la pantalla de inicio dependiendo de si el usuario está autenticado o no.
+    val startDestination =
+        if (comprobarUsuario())
+            "list"  // Si está autenticado, ir a la lista de hábitos
+        else
+            "login" // Si no está autenticado, ir a la pantalla de login
 
-    NavHost(navController = navController, startDestination = "list") {
-        composable("list") { FilmListScreen(navController, viewModel) }
-        composable("data/{filmIndex}") { backStackEntry ->
+
+    NavHost(navController = navController, startDestination = startDestination) {
+        // Ruta para la pantalla de registro
+        //composable("register") {
+        //    RegisterScreen(navController, authViewModel)
+        //}
+        composable("list") {
+            if (comprobarUsuario())
+                FilmListScreen(navController, viewModel)
+            else
+                LoginScreen(navController, authViewModel)
+        }
+        composable("data/{filmIndex}") {
+            backStackEntry ->
             val filmIndex = backStackEntry.arguments?.getString("filmIndex")
             filmIndex?.let {
-                FilmDataScreen(navController, filmIndex = it, viewModel)
+                if (comprobarUsuario())
+                    FilmDataScreen(navController, filmIndex = it, viewModel)
+                else
+                    LoginScreen(navController, authViewModel)
             }
         }
         composable("edit/{filmIndex}") { backStackEntry ->
             val filmIndex = backStackEntry.arguments?.getString("filmIndex")
             filmIndex?.let {
-                FilmEditScreen(navController, filmIndex = it)
+                if (comprobarUsuario())
+                    FilmDataScreen(navController, filmIndex = it, viewModel)
+                else
+                    FilmEditScreen(navController, filmIndex = it)
             }
         }
         composable("about") { AboutScreen(navController) }
